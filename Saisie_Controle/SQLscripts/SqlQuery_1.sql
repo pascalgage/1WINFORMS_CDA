29da345e6b2da82b0eb0281b7f367ecfd978af69
pascalgage <72741160+pascalgage@users.noreply.github.com>
2021-09-15 22:39:17 +0200

    ok bdd

---
+++ Saisie_Controle/SQLscripts/SqlQuery_1.sql
@@ -0,0 +1,13 @@
+﻿DROP TABLE IF EXISTS
+clients
+GO
+
+
+CREATE TABLE clients
+(
+	client_id int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	client_nom VARCHAR(30) NOT NULL,
+	client_date DATE NOT NULL,
+	client_montant VARCHAR(50) NOT NULL,
+	client_codePostal VARCHAR(50) NOT NULL
+)
