864cfafe2e871c3980fb447a4c53a50090e91aa2
Pascal GAGE <PGage@arfp.asso.fr>
2021-09-15 08:15:35 +0200

    essai push mercredi

--- Saisie_Controle/Form1.cs
+++ Saisie_Controle/Form1.cs
@@ -18,12 +18,12 @@ namespace Saisie_Controle
             InitializeComponent();
         }
 
-
-        private void button1_Click(object sender, EventArgs e)
+        private void Validation()
         {
-            if ((Regex.Match(textBox1.Text, "^[a-zA-Z]*$").Success) && (textBox1.Text.Length <= 30) && (textBox1.Text!=""))
+            
+            if ((Regex.Match(textBox1.Text, "^[a-zA-Z]*$").Success) && (textBox1.Text.Length <= 30) && (textBox1.Text != ""))
             {
-                if ((Regex.Match(textBox2.Text, "^[0-9]*$").Success)&&(textBox2.Text.Length==8))
+                if ((Regex.Match(textBox2.Text, "^[0-9]*$").Success) && (textBox2.Text.Length == 8))
                 {
                     if (Regex.Match(textBox3.Text, "^[0-9]*$").Success)
                     {
@@ -62,52 +62,23 @@ namespace Saisie_Controle
                 MessageBox.Show("ERROR le nom ne doit comporter que des lettres");
                 errorProviderNom.SetError(textBox1, "ERROR le nom ne doit comporter que des lettres");
             }
-
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Validation();
+
+        }
+        //bool successful = DateTime.TryParseExact
+        //("20201205", "yyyyMMdd", null, System.Globalization.DateTimeStyles.AllowLeadingWhite, out dateARecupere);
         private void textBox1_Leave(object sender, EventArgs e)
         {
-            if ((Regex.Match(textBox1.Text, "^[a-zA-Z]*$").Success) && (textBox1.Text.Length <= 30) && (textBox1.Text != ""))
-            {
-                if ((Regex.Match(textBox2.Text, "^[0-9]*$").Success))
-                {
-                    if (Regex.Match(textBox3.Text, "^[0-9]*$").Success)
-                    {
-                        if ((textBox4.Text.Length < 6) && (Regex.Match(textBox4.Text, "^[0-9]*$").Success))
-                        {
-                            errorProviderNom.Clear();
-                            //MessageBox.Show("NOM:" + textBox1.Text + "\nDATE:" + textBox2.Text + "\nMONTANT:" + textBox3.Text +
-                                 //"\nCODE POSTAL:" + textBox4.Text);
-                        }
-                        else
-                        {
-                            MessageBox.Show("ERROR le code postal = maximun 5 chiffres");
-                        }
-
-                    }
-                    else
-                    {
-                        MessageBox.Show("ERROR le montant doit être numérique");
-                    }
-
-                }
-                else
-                {
-                    MessageBox.Show("ERROR la date doit être de forme JJMMAAAA");
-                }
-
-            }
-            else
-            {
-                MessageBox.Show("ERROR le nom ne doit comporter que des lettres");
-                errorProviderNom.SetError(textBox1, "ERROR le nom ne doit comporter que des lettres");
-            }
-
+            //Validation();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            Validation();
         }
     }
 }
