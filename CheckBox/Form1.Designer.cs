223303647ae4f6d46e6415591b953192cea8a281
Pascal GAGE <PGage@arfp.asso.fr>
2021-09-16 15:37:24 +0200

    colorbox en cours

---
+++ CheckBox/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿
+namespace CheckBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textchoix = new System.Windows.Forms.TextBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.radioButton11 = new System.Windows.Forms.RadioButton();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBoxChoix = new System.Windows.Forms.GroupBox();
+            this.majmintext = new System.Windows.Forms.CheckBox();
+            this.colortext = new System.Windows.Forms.CheckBox();
+            this.fontcolor = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.labelresult = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBoxChoix.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tapez votre texte";
+            // 
+            // textchoix
+            // 
+            this.textchoix.Location = new System.Drawing.Point(27, 47);
+            this.textchoix.Name = "textchoix";
+            this.textchoix.Size = new System.Drawing.Size(365, 20);
+            this.textchoix.TabIndex = 1;
+            this.textchoix.TextChanged += new System.EventHandler(this.textChoix_TextChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(29, 30);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(57, 17);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Rouge";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(29, 53);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(44, 17);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Vert";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(29, 76);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(46, 17);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Bleu";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(36, 29);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(57, 17);
+            this.radioButton7.TabIndex = 8;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Rouge";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(36, 52);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(52, 17);
+            this.radioButton8.TabIndex = 9;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Blanc";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(36, 75);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(44, 17);
+            this.radioButton9.TabIndex = 10;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "Noir";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(6, 38);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(78, 17);
+            this.radioButton10.TabIndex = 11;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "Minuscules";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton11
+            // 
+            this.radioButton11.AutoSize = true;
+            this.radioButton11.Location = new System.Drawing.Point(6, 75);
+            this.radioButton11.Name = "radioButton11";
+            this.radioButton11.Size = new System.Drawing.Size(78, 17);
+            this.radioButton11.TabIndex = 12;
+            this.radioButton11.TabStop = true;
+            this.radioButton11.Text = "Majuscules";
+            this.radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(91, 221);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(521, 20);
+            this.maskedTextBox1.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton10);
+            this.groupBox1.Controls.Add(this.radioButton11);
+            this.groupBox1.Location = new System.Drawing.Point(544, 301);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(94, 112);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Casse";
+            // 
+            // groupBoxChoix
+            // 
+            this.groupBoxChoix.Controls.Add(this.majmintext);
+            this.groupBoxChoix.Controls.Add(this.colortext);
+            this.groupBoxChoix.Controls.Add(this.fontcolor);
+            this.groupBoxChoix.Enabled = false;
+            this.groupBoxChoix.Location = new System.Drawing.Point(606, 22);
+            this.groupBoxChoix.Name = "groupBoxChoix";
+            this.groupBoxChoix.Size = new System.Drawing.Size(158, 130);
+            this.groupBoxChoix.TabIndex = 15;
+            this.groupBoxChoix.TabStop = false;
+            this.groupBoxChoix.Text = "Choix";
+            // 
+            // majmintext
+            // 
+            this.majmintext.AutoSize = true;
+            this.majmintext.Location = new System.Drawing.Point(17, 74);
+            this.majmintext.Name = "majmintext";
+            this.majmintext.Size = new System.Drawing.Size(55, 17);
+            this.majmintext.TabIndex = 2;
+            this.majmintext.Text = "Casse";
+            this.majmintext.UseVisualStyleBackColor = true;
+            // 
+            // colortext
+            // 
+            this.colortext.AutoSize = true;
+            this.colortext.Location = new System.Drawing.Point(17, 51);
+            this.colortext.Name = "colortext";
+            this.colortext.Size = new System.Drawing.Size(135, 17);
+            this.colortext.TabIndex = 1;
+            this.colortext.Text = "Couleur des caractères";
+            this.colortext.UseVisualStyleBackColor = true;
+            // 
+            // fontcolor
+            // 
+            this.fontcolor.AutoSize = true;
+            this.fontcolor.Location = new System.Drawing.Point(17, 27);
+            this.fontcolor.Name = "fontcolor";
+            this.fontcolor.Size = new System.Drawing.Size(101, 17);
+            this.fontcolor.TabIndex = 0;
+            this.fontcolor.Text = "Couleur du fond";
+            this.fontcolor.UseVisualStyleBackColor = true;
+            this.fontcolor.CheckedChanged += new System.EventHandler(this.fontcolor_CheckedChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton4);
+            this.groupBox3.Controls.Add(this.radioButton5);
+            this.groupBox3.Controls.Add(this.radioButton6);
+            this.groupBox3.Location = new System.Drawing.Point(91, 300);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(135, 113);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Fond";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.radioButton7);
+            this.groupBox4.Controls.Add(this.radioButton8);
+            this.groupBox4.Controls.Add(this.radioButton9);
+            this.groupBox4.Location = new System.Drawing.Point(299, 301);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 112);
+            this.groupBox4.TabIndex = 17;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Caractères";
+            // 
+            // labelresult
+            // 
+            this.labelresult.Location = new System.Drawing.Point(27, 118);
+            this.labelresult.Name = "labelresult";
+            this.labelresult.Size = new System.Drawing.Size(420, 26);
+            this.labelresult.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelresult);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBoxChoix);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textchoix);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBoxChoix.ResumeLayout(false);
+            this.groupBoxChoix.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textchoix;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton11;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBoxChoix;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label labelresult;
+        private System.Windows.Forms.CheckBox majmintext;
+        private System.Windows.Forms.CheckBox colortext;
+        private System.Windows.Forms.CheckBox fontcolor;
+    }
+}
+
---
+++ CheckBox/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textChoix_TextChanged(object sender, EventArgs e)
+        {
+            groupBoxChoix.Enabled = true;
+            labelresult.Text= textchoix.Text;
+
+        }
+
+        private void fontcolor_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
