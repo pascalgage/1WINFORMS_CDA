4b5a75ddcee855b3c594c0a40c042814aca2b4cc
Gage <72741160+pascalgage@users.noreply.github.com>
2021-09-22 19:28:38 +0200

    ok

---
+++ Emprunts/ClassLibraryCalculFinancier/CalF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCalculFinancier
+{
+    public class Calcul
+    {
+        public static void NombreMensualites(string _nombreMensualites)
+        {
+            
+        }
+    }
+}
--- Emprunts/Form1.cs
+++ Emprunts/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ClassLibraryCalculFinancier;
 namespace Emprunts
 {
     public partial class Form1 : Form
@@ -20,18 +20,34 @@ namespace Emprunts
         private void hScrollBarDuree_Scroll(object sender, ScrollEventArgs e)
         {
             labelDure.Text = hScrollBarDuree.Value.ToString();
+            
         }
 
         private void buttonOk_Click(object sender, EventArgs e)
         {
+            double mensualitesPrevues = double.Parse(labelDure.Text);
+
+            if (listBoxPeriodicite.SelectedItem == listBoxPeriodicite.Items[3])
+            {
+                double mensualiteNombr = mensualitesPrevues / 4;
+            }
+
+
+
+
             if (radioButt8perc.Checked)
             {
-                double k = double.Parse(textBoxCapitalEmpr.Text);
-                double tx = 8;
+                double k = double.Parse(textBoxCapitalEmpr.Text);// k Capital
+                double tx = 8;// tx TAUX ANNUEL
+
+                
+
+                
+
                 double m = double.Parse(labelDure.Text);
                 double result = 0;
 
-                result = ((k * tx/100)/m);
+                result = ((k * tx/100)*m);
                 labelRembousement.Text = result.ToString();
 
 
@@ -42,6 +58,8 @@ namespace Emprunts
             {
 
             }
+
+            
             
         }
     }
